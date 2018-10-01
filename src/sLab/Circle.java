f1d28b2f86ac1e8d67f82834a208f1a15b693323
BT_1N3_23 <dyau4570@bths.edu>
2018-10-01 11:44:54 -0400

    added local variables

--- src/sLab/Circle.java
+++ src/sLab/Circle.java
@@ -5,17 +5,14 @@ import java.lang.Math;
 public abstract class Circle extend Shape {
 
    private int radius;
-
    public Circle(int radius) {
        super("Circle");
        this.radius = radius;
     }
-
     public double getArea() {
         double y = this.radius*this.radius*Math.PI;
         return y;
     }
-
     public double getPerimeter() {
         double v = this.radius * Math.PI * 2;
         return v;
