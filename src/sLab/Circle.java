cf3dafa211bbbcd406d09b87583745c2a2d40f23
BT_1N3_23 <dyau4570@bths.edu>
2018-10-01 11:40:25 -0400

    fixed a few things

--- src/sLab/Circle.java
+++ src/sLab/Circle.java
@@ -2,20 +2,20 @@ package sLab;
 
 import java.lang.Math;
 
-public abstract class Circle {
+public abstract class Circle extend Shape {
 
    private int radius;
 
    public Circle(int radius) {
-       super(Circle);
+       super("Circle");
        this.radius = radius;
     }
 
     public double getArea() {
-        return radius*radius*Math.PI;
+        return this.radius*this.radius*Math.PI;
     }
 
     public double getPerimeter() {
-       return radius*Math.PI*2;
+       return this.radius*Math.PI*2;
     }
 }
